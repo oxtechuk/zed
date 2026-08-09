aff2db7e468a3cafc8441028265442776faea8ea
Ox tech <oxtech.uk@gmail.com>
2026-08-09 16:32:28 +0200

    س

--- resources/react/i18n.ts
+++ resources/react/i18n.ts
@@ -1,21 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import HttpBackend from "i18next-http-backend";
+import ar from "../../public/locales/ar.json";
+import en from "../../public/locales/en.json";
 
 const savedLang = localStorage.getItem("language") || "ar";
-const base = import.meta.env.BASE_URL || "/";
-const cleanBase = base.endsWith("/") ? base : `${base}/`;
 
 i18n
-  .use(HttpBackend)
   .use(initReactI18next)
   .init({
-    lng: savedLang,
-    fallbackLng: "en",
-    debug: true,
-    backend: {
-      loadPath: `${cleanBase}locales/{{lng}}.json`,
+    resources: {
+      ar: { translation: ar },
+      en: { translation: en },
     },
+    lng: savedLang,
+    fallbackLng: "ar",
     interpolation: {
       escapeValue: false,
     },
